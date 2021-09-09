8e65e8ae36cd7bf5ee4ddce48f0c87123297950b
ormissia <ormissia@outlook.com>
2021-09-09 13:10:21 +0800

    :art: update make script

--- eas-go-service/build.sh
+++ eas-go-service/build.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+# 代码中服务目录在项目跟目录中的文件夹名称
+SERVICE_DIR_NAME="main"
+
 # 根据传入参数进行编译，参数1：服务名称，即每个服务文件夹名称
-function GoBuild {
-  echo go building "$1" ...
+function Build {
+  echo building "$1" ...
 # 编译
-  go build -o "$PROJECT_ROOT_DIR"/target/"$1" "$1"/main.go
+  if [ "$2" == "binary" ]; then
+    go build -o "$PROJECT_ROOT_DIR"/target/"$1" "$SERVICE_DIR_NAME"/"$1"/main.go
+  elif [ "$2" == "docker-build" ]; then
+    docker build -t ormissia/"$1" --build-arg SERVICE_NAME="$1" --progress=plain .
+  elif [ "$2" == "docker-push" ]; then
+    echo docker-push
+  elif [ "$2" == "docker-clean" ]; then
+    echo docker-clean
+  else
+    echo "incorrect compile type $1"
+    exit
+    fi
 # 判断编译是否成功
   if [ $? -eq 0 ];then
     echo build "$1" successful
@@ -25,22 +39,19 @@ BUILD_SUCCESSFUL_COUNT=0
 # 编译失败数量
 BUILD_FAILED_COUNT=0
 
+SERVICES=$(ls $SERVICE_DIR_NAME)
 
-# 进入服务目录
-cd main || exit
-
-SERVICES=$(ls)
-
-# 当参数个数不为0时，需要编译的服务为传入的参数
-if [ $# -ne 0 ]; then
-    SERVICES=$*
+# sh build.sh binary/docker $(service)
+# 当传入参数为2个时,第二个为服务名
+if [ $# -eq 2 ]; then
+  SERVICES=$2
 fi
 
 # 遍历所有文件夹名称，即所有服务
 for directory in $SERVICES
 do
 # 调用编译函数
-  GoBuild "$directory"
+  Build "$directory" "$1"
 done
 
 # 输出编译结果
--- eas-go-service/go.mod
+++ eas-go-service/go.mod
@@ -43,6 +43,8 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.2 // indirect
 	github.com/json-iterator/go v1.1.11 // indirect
+	github.com/kr/pretty v0.2.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
